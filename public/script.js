5a4fac93944a5d102d69954dd5f3f8c6aea938f0
Terry Chiem <terrychiem@gmail.com>
2020-08-22 23:49:32 -0700

    implement chat server

--- public/script.js
+++ public/script.js
@@ -1,27 +1,53 @@
 const socket = io.connect('http://localhost:3000');
 
+const userName = prompt("Enter user name");
 
+socket.emit('join', { userName });
 
+const chatText = document.getElementById('chatText');
+const chatForm = document.getElementById('chat');
+chatForm.addEventListener('submit', handleSubmit);
 
-const box = document.querySelector('.box');
-box.addEventListener('click', handleClick);
-
-function handleClick(e) {
-  const data = {
-    x: e.x,
-    y: e.y
-  }
-  socket.emit('click', data);
-  console.log('click!');
+function handleSubmit(e) {
+  e.preventDefault();
+  socket.emit('client message', { data: chatText.value });
+  chatText.value = '';
 }
 
 const messages = document.querySelector('.messages');
+const userList = document.querySelector('.user-list');
+
+// socket events
+socket.on('user list', createUserList);
+socket.on('server message', addMessage);
+socket.on('update user list', updateUserList);
+socket.on('remove user', removeUser);
+
+// socket event functions
+function createUserList(users) {
+  for (let user of users) {
+    updateUserList(user);
+  }
+}
+
+function updateUserList(userName) {
+  const user = document.createElement('li');
+  user.innerText = userName;
+  userList.appendChild(user);
+}
 
 function addMessage(string) {
-  console.log('click received from server')
   const message = document.createElement('li');
   message.innerText = string;
   messages.appendChild(message);
 }
 
-socket.on('clicked', addMessage);
+function removeUser(userName) {
+  const userNodes = userList.childNodes;
+  for (let user of userNodes) {
+    if (user.innerText === userName) {
+      userList.removeChild(user);
+      break;
+    }
+  }
+}
--- server.js
+++ server.js
@@ -7,21 +7,45 @@ const server = app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
 
-// serve public folder files
+// TEMP: Replace with routes to connect to front end
 app.use(express.static('public'));
 
 // establish socket
 const io = socket(server);
-io.sockets.on('connection', newConnection);
+io.sockets.on('connect', newConnection);
+
+// chat users
+const users = {};
 
 function newConnection(socket) {
   console.log('new connection: ', socket.id);
 
-  socket.on('click', clickMsg);
-  function clickMsg(data) {
-    console.log('click from: ', socket.id);
-    const msg = `${socket.id} clicked at: ${data.x}, ${data.y}`
-    // socket.broadcast.emit('clicked', msg);
-    io.sockets.emit('clicked', msg);
+  // send out all current users
+  socket.emit('user list', Object.values(users));
+
+  // handle username on join
+  socket.on('join', nameUser);
+  function nameUser({ userName }) {
+    users[socket.id] = userName;
+    io.sockets.emit('server message', `${userName} joined the chat.`);
+    io.sockets.emit('update user list', userName);
+  }
+
+  // handle sending of messages
+  socket.on('client message', chatMsg);
+  function chatMsg({ data }) {
+    const msg = `${users[socket.id]}: ${data}`;
+    io.sockets.emit('server message', msg);
+  }
+
+  // handle disconnect
+  socket.on('disconnect', handleDisconnect);
+  function handleDisconnect() {
+    console.log('disconnected: ', socket.id);
+    const userName = users[socket.id];
+    io.sockets.emit('server message', `${userName} left the chat.`);
+    io.sockets.emit('remove user', userName);
+    delete users[socket.id];
   }
 }
+
